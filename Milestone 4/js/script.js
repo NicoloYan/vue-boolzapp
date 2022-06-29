290b58b1b864aa98cc0201afda8ccafcdb421c87
NicoloYan <nico.yan@outlook.it>
2022-06-29 17:13:29 +0200

    Proceeding with Milestone 4

--- Milestone 4/js/script.js
+++ Milestone 4/js/script.js
@@ -3,8 +3,6 @@ var app = new Vue (
 		el: '#root',
 		data: {
 			currentActiveContact: 0,
-			newMessage: '',
-			stringToSearch: '',
 			contacts: [
 
 				{
@@ -91,33 +89,10 @@ var app = new Vue (
 					],
 				},
 			]
-		}, 
-		
-		methods: {
-
+		}, methods: {
 			selectContact(index) {
 				this.currentActiveContact = index;
 			},
-
-			sendMessage() {
-                this.contacts[this.currentActiveContact].messages.push({
-                    text: this.newMessage,
-                    date: '',
-					status: 'sent'
-                })
-                this.newMessage = '',
-				setTimeout(this.autoMessage, 1000) 
-
-            },
-
-			autoMessage() {
-				this.contacts[this.currentActiveContact].messages.push({
-                    text: 'ok',
-                    date: '',
-					status: 'received'
-                })
-			},
-
-		}	
+		}
 	}
 )
